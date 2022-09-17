d40d4f3859216e4f26aa781d948af27fe5905d72
Anddd7 <liaoad_space@sina.com>
2022-09-17 16:56:25 +0800

    create nat

--- examples/isolated-vpcs-routable-traffic/gcp/main.tf
+++ examples/isolated-vpcs-routable-traffic/gcp/main.tf
@@ -101,3 +101,28 @@ module "private_vm_2" {
     network_tags = ["allow-alltraffic"]
   }
 }
+
+# nat
+
+resource "google_compute_router" "public_router" {
+  name    = "public-router"
+  network = google_compute_network.public_vpc.id
+  region  = google_compute_subnetwork.public_subnet_1.region
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "public_nat" {
+  name                               = "public-router-nat"
+  router                             = google_compute_router.public_router.name
+  region                             = google_compute_router.public_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
